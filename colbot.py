5c37f9ec0168a28e2ff12e454816de7bfb43e4d5
Colby McClure <colbymc0801@gmail.com>
2025-02-03 20:26:33 -0600

    colbot.py   - Documented what I can for now   - Added functionality to send random quotes   - Changed structure of the message handler to be match-case

--- colbot.py
+++ colbot.py
@@ -1,11 +1,27 @@
 import discord
 import os
+import requests
+import json 
 from dotenv import load_dotenv
 
+# Load the environment file that contains the Colbot token
 load_dotenv("token.env")
 
+# Retrieve the token from the environment
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+# This function is used to retrieve a random quote using the zenquotes api and return it
+def get_quote():
+
+    # Create the response using the requests.get method and the URL
+    response = requests.get("https://zenquotes.io/api/random")
+
+    # Save the JSON array that gets downloaded from the API call
+    json_data = json.loads(response.text)
+
+    # Fix the struture and formatting of the quote and return it
+    quote = json_data[0]['q'] + "\n  -" + json_data[0]['a']
+    return quote 
 
 if not TOKEN:
     print("ERROR: Bot token was not found in .env file.")
@@ -24,7 +40,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.content.startswith('Colbot'):
-        await message.channel.send('It is I, the holy Colbot. Harbinger of Worlds...')
+
+    match True: 
+
+        case _ if message.content == 'help':
+            await message.channel.send('Here are the following commands: \n  !hi: Colbot greets you\n  !inspire: Colbot will send you an inspirational quote')
+        case _ if message.content.startswith('!hi'):
+            await message.channel.send('It is I, the holy Colbot. Harbinger of Worlds...')
+
+        case _ if message.content.startswith('!inspire'):
+            quote = get_quote()
+            await message.channel.send(quote)
 
 client.run(TOKEN)
