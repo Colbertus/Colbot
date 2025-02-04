e2bb90d517ed303f64ece468177413943664a650
Colby McClure <colbymc0801@gmail.com>
2025-02-04 16:06:02 -0600

    colbot.py :: added some commands for Marvel Rivals API

--- colbot.py
+++ colbot.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json 
 from dotenv import load_dotenv
+import mr_api as mr
 
 # Load the environment file that contains the Colbot token
 load_dotenv("token.env")
@@ -16,6 +17,7 @@ def inspo_quote():
     # Create the response using the requests.get method and the URL
     try:
         response = requests.get("https://zenquotes.io/api/random", timeout = 10)
+        response.raise_for_status()
 
         # Save the JSON array that gets downloaded from the API call
         json_data = json.loads(response.text)
@@ -49,11 +51,30 @@ async def on_message(message):
 
         case _ if message.content == 'help':
             await message.channel.send('Here are the following commands: \n  !hi: Colbot greets you\n  !inspire: Colbot will send you an inspirational quote')
+
         case _ if message.content.startswith('!hi'):
             await message.channel.send('It is I, the holy Colbot. Harbinger of Worlds...')
 
         case _ if message.content.startswith('!inspire'):
             quote = inspo_quote()
             await message.channel.send(quote)
+        
+        case _ if message.content.startswith('!mrGift'):
+            gift = mr.gift_codes()
+            await message.channel.send(gift)
+
+        case _ if message.content.startswith('!mrID'):
+            arguments = message.content.split()
+
+            if len(arguments) != 2:
+                await message.channel.send('Usage:\n  Need to provide the command along with the one argument that is required\n  Example usage: !mrID {username}')
+                return
+            
+            username = arguments[1]
+
+            playerID = mr.player_id(username)
+            await message.channel.send(playerID)
+
+
 
 client.run(TOKEN)
