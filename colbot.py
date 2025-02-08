4996bfba5fa03bdd0c21fe56b1e45a589fae8b42
Colby McClure <colbymc0801@gmail.com>
2025-02-08 13:51:36 -0600

    colbot & mr_api - Added code functionality to pull player stats given a username or ID
    - Fixed some of the error handling with the requests

--- colbot.py
+++ colbot.py
@@ -50,7 +50,16 @@ async def on_message(message):
     match True: 
 
         case _ if message.content == 'help':
-            await message.channel.send('Here are the following commands: \n  !hi: Colbot greets you\n  !inspire: Colbot will send you an inspirational quote')
+            helpMessage = (
+                "## Here are the Following Commands: ##\n" 
+                "- `!hi`: *Colbot greets you*\n"
+                "- `!inspire`: *Colbot will send you an inspirational quote*\n"
+                "- `!mrGift`: *Colbot will send you each valid Marvel Rivals code"
+                " that can unlock a previously locked skin for a hero along with their"
+                " respective expiration dates*\n"
+                "- `mrID`: *Colbot will provide a Marvel Rivals username given a valid ID*"
+            )
+            await message.channel.send(helpMessage)
 
         case _ if message.content.startswith('!hi'):
             await message.channel.send('It is I, the holy Colbot. Harbinger of Worlds...')
@@ -67,14 +76,30 @@ async def on_message(message):
             arguments = message.content.split()
 
             if len(arguments) != 2:
-                await message.channel.send('Usage:\n  Need to provide the command along with the one argument that is required\n  Example usage: !mrID {username}')
+                await message.channel.send('Usage:\n  Need to provide the command along with the one argument that is required\n  Example usage: !mrID {user name}')
                 return
             
             username = arguments[1]
 
             playerID = mr.player_id(username)
             await message.channel.send(playerID)
+        
+        case _ if message.content.startswith('!mrStats'):
+            arguments = message.content.split()
 
+            if len(arguments) != 2:
+                await message.channel.send('Usage:\n  Need to provide the command along with the one argument that is required\n  Example usage: !mrStats {player id}')
 
+            ID = arguments[1]
+
+            if any(char.isalpha() for char in ID):
+                idInfo = mr.player_id(ID)
+                arguments = idInfo.split()
+                actualID = arguments[3]
+                stats = mr.player_stats(actualID)
+                await message.channel.send(stats)
+            else:
+                stats = mr.player_stats(ID)
+                await message.channel.send(stats)
 
 client.run(TOKEN)
--- mr_api.py
+++ mr_api.py
@@ -8,7 +8,11 @@ def gift_codes():
 
     json_data = json.loads(response.text)
 
-    giftCodes = "Reward: " + json_data[0]['rewards'] + "\nCode: " + json_data[0]['code'] + "\nExpiration: " + json_data[0]['expiringDate']
+    giftCodes = (
+      "Reward: " + json_data[0]['rewards'] +
+      "\nCode: " + json_data[0]['code'] +
+      "\nExpiration: " + json_data[0]['expiringDate']
+    )
 
     return giftCodes
 
@@ -25,9 +29,46 @@ def player_id(name):
        json_data = json.loads(response.text)
 
 
-       playerID = "Name: " + json_data['name'] + "\nID: " + json_data['id']
+       playerID = (
+        "Name: " + json_data['name'] + 
+        "\nID: " + json_data['id']
+       )
        
        return playerID
     except requests.exceptions.RequestException as e:
        error = f"An error occurred: {e}"
        return error 
+
+def player_stats(ID):
+
+  try:
+
+    response = requests.get('https://mrapi.org/api/player/' + ID)
+    response.raise_for_status()
+
+    json_data = json.loads(response.text)
+
+    playerName = json_data['player_name']
+    playerID = json_data['player_uid']
+    playerAchievements = json_data['achievements_completed']
+    playerLevel = json_data['stats']['level']
+    playerRank = json_data['stats']['rank']['rank']
+    ranked_kdr = json_data['stats']['ranked']['kdr']
+    unranked_kdr = json_data['stats']['unranked']['kdr']
+
+    stats = (
+      "## Stats for player: " + playerName + 
+      " (ID: " + playerID + ") ##\n" 
+      "- **Number of Achievements:** " + "*" + playerAchievements + "*" + "\n"
+      "- **Player Level:** " + "*" + playerLevel + "*" + "\n"
+      "- **Player Rank:** " + "*" + playerRank + "*" +"\n"
+      "- **Unranked K/D:** " + "*" + unranked_kdr + "*" + "\n"
+      "- **Ranked K/D:** " + "*" + ranked_kdr + "*" +"\n"
+    )
+
+    return stats
+
+  except requests.exceptions.RequestException as e:
+    error = f"An error occurred: {e}"
+    return error
+
